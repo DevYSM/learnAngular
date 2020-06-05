9782edb6e568b9f8e57b263ac0eee9e27f41ac89
DevYSM <devysm2019@gmail.com>
2020-06-05 22:56:22 +0200

    Learn Services Cli Command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MyFirstComponent } from './my-first/my-first.component';
 import { DBServices } from './db.services';
+import { ItshareApiService } from './services/itshare-api.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { DBServices } from './db.services';
   imports: [
     BrowserModule,
    ],
-  providers: [DBServices], // register the provide of DB Services
+  providers: [
+    DBServices,
+    ItshareApiService
+  ], // register the provide of DB Services
   bootstrap: [AppComponent]
 }) 
 export class AppModule { }
